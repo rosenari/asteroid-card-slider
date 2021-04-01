6c66ff4da1bbeb82ab751c04a0e99e1ed8de86b1
임장순 <rosenari88@gmail.com>
2021-04-02 00:44:17 +0900

    Add / #4 #8 / createPoint, resizeHandler in Cardslider class

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ class Cardslider {
         try {
             //Init Dom Element;
             this.elem = document.getElementById(elem);
+            this.container = null;
+            this.points = [];
 
             //validate options
             OptionFactory.validate(options);
@@ -21,7 +23,7 @@ class Cardslider {
                 width: options.width || "360px",
                 height: options.height || "150px",
                 isAuto: options.isAuto || false,
-                isPoint: options.isPoint || false,
+                isPoint: options.isPoint || true,
                 images: options.images || []
             }
 
@@ -37,6 +39,7 @@ class Cardslider {
     public
     */
     render() {
+        /** createCard */
         let container = document.createElement('div');
         container.style.width = this.options.width;
         container.style.height = this.options.height;
@@ -46,7 +49,14 @@ class Cardslider {
             let card = this.#createCard(image);
             container.appendChild(card);
         }
+        this.container = container;
         this.elem.appendChild(container);
+
+        /** createPoint */
+        if (this.options.isPoint) {
+            this.resizeHandler();
+            window.addEventListener('resize', this.resizeHandler);
+        };
     }
 
     /**
@@ -57,13 +67,62 @@ class Cardslider {
     #createCard(imgSrc) {
         let card = document.createElement('div');
         card.classList.add("card");
-        card.style.width = "33%";
         card.innerHTML = `<img src="${imgSrc}" style="height:100%" />`;
 
         return card;
     }
 
+    /**
+    method createPoint in Cardslider class
+    private
+    @returns { Element } return PointElement
+    */
+    #createPoint() {
+
+        let pointbox = document.createElement('div');
+        pointbox.classList.add('pointbox');
+        pointbox.style.width = this.options.width;
+        pointbox.style.height = "20px";
+
+        for (let i = 0; i < this.points.length; i++) {
+            let point = document.createElement('div');
+            point.classList.add('point');
+
+            if (this.points[i].active) point.classList.add('active');
+
+            pointbox.appendChild(point);
+        }
+
+        this.elem.appendChild(pointbox);
+    }
+
+    /**
+     function resizeHandler in Cardslider class
+     public
+     */
+    resizeHandler = () => {
+        //Calc pointNum
+        let pointNum = Math.ceil(this.container.scrollWidth / this.container.clientWidth);
+
+        let temp = new Array(pointNum);
+
+        for (let i = 0; i < temp.length; i++) {
+            temp[i] = {
+                x: this.container.clientWidth * i,
+                active: false,
+            };
+        }
+
+        if (temp.length > 0) temp[0].active = true;
+
+        if (!this.points || JSON.stringify(temp) != JSON.stringify(this.points)) {
+            this.points = temp;
+            this.#createPoint();
+        }
+    }
+
 }
 
-
 window.Cardslider = Cardslider;
+
+export default Cardslider;
